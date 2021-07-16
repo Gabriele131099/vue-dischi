f31cc5001e99300445853088691f999515ef6413
gabriele131099 <gabrisanna131099@gmail.com>
2021-07-16 15:19:03 +0200

    struttura

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,10 +2,9 @@
     <div>
         <nav class="nav-first">
             <img src="@/assets/spotify-logo.png" alt="">
-        </nav>
-        <div>
+        <div class="container-genre">
 
-            <select class="genre" 
+            <select class="genre" placeholder="All" 
             v-model="selectGenre">
                 <option value="All">All</option>
                 <option v-for="(genre , index) in tmpGenre"
@@ -20,6 +19,7 @@
 
 
         </div>
+        </nav>
     </div>
 </template>
 <script>
@@ -38,11 +38,31 @@ export default {
 }
 </script>
 <style lang="scss">
-
+    .container-genre{
+        margin: 15px;
+            button{
+                padding: 5px 10px;
+                border-radius: 15px;
+                background-color: rgba(38, 129, 73, 0.5);
+                color: whitesmoke;
+                border: 1px solid rgb(22, 22, 71);
+            }
+    }
+    .genre{
+        background-color: rgb(245, 244, 244);
+        border-radius: 10px;
+        margin: 15px;
+        padding: 5px ;
+        outline: none;
+    }
+ 
   
   .nav-first
   {
       background-color: rgb(17, 34, 49) ;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
     img {
         width: 100px;
         padding: 15px;
